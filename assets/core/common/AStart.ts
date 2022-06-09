30e4f51117d38d6dec86bd4d9993300542107db1
eathonq <vangagh@live.cn>
2022-06-09 14:42:45 +0800

    A Start 问题修复，Demo版本完善

--- assets/core/common/AStart.ts
+++ assets/core/common/AStart.ts
@@ -1,20 +1,28 @@
 // 参考 https://github.com/melkir/A-Star-Python
 
 //#region Graph
-export class SimpleGraph {
-    edges: any[][];
-    constructor() {
-        this.edges = [];
-    }
-    neighbors(id: { x: number; y: number; }) {
-        return this.edges[id.x][id.y] || [];
-    }
-}
 
+// export class SimpleGraph {
+//     edges: any[][];
+//     constructor() {
+//         this.edges = [];
+//     }
+//     neighbors(id: { x: number; y: number; }) {
+//         return this.edges[id.x][id.y] || {};
+//     }
+// }
+
+/** 方格数据 */
 export class SquareGrid {
     private width: number;
     private height: number;
     private walls: {};
+    /**
+     * 初始化方格数据
+     * @param width 宽度
+     * @param height 高度
+     * @param walls 墙体数据
+     */
     constructor(width: number, height: number, walls?: { x: number, y: number }[]) {
         this.width = width;
         this.height = height;
@@ -36,7 +44,7 @@ export class SquareGrid {
 
     neighbors(id: { x: number; y: number; }) {
         const { x, y } = id;
-        const results = [{ x: x + 1, y: y }, { x: x, y: y - 1 }, { x: x - 1, y: y }, { x: x, y: y + 1 }];
+        const results = [{ x: x + 1, y }, { x, y: y - 1 }, { x: x - 1, y }, { x, y: y + 1 }];
         if ((x + y) % 2 == 0) {
             results.reverse();
         }
@@ -44,8 +52,16 @@ export class SquareGrid {
     }
 }
 
+/** 带权重的方格数据 */
 export class GridWithWeights extends SquareGrid {
     private weights: {};
+    /**
+     * 初始化带权重的方格数据
+     * @param width 宽度
+     * @param height 高度
+     * @param walls 墙体数据
+     * @param weights 权重数据，格式为 { x: number, y: number, weight: number }，weight 默认为1
+     */
     constructor(width: number, height: number, walls?: { x: number, y: number }[], weights?: { x: number, y: number, weight: number }[]) {
         super(width, height, walls);
         this.weights = weights;
@@ -65,8 +81,11 @@ export class GridWithWeights extends SquareGrid {
 //#endregion
 
 //#region Queue
+
+/** 队列 */
 export class Queue {
     private elements: any[];
+    /** 队列 */
     constructor() {
         this.elements = [];
     }
@@ -84,8 +103,10 @@ export class Queue {
     }
 }
 
+/** 优先队列 */
 export class PriorityQueue {
     private elements: any[];
+    /** 优先队列 */
     constructor() {
         this.elements = [];
     }
@@ -95,17 +116,14 @@ export class PriorityQueue {
     }
 
     put(item: { x: number; y: number; }, priority: number) {
-        let added = false;
+        // 最小priority 排在前面
         for (let i = 0; i < this.elements.length; i++) {
-            if (this.elements[i].priority > priority) {
+            if (this.elements[i].priority >= priority) {
                 this.elements.splice(i, 0, { item, priority });
-                added = true;
-                break;
+                return;
             }
         }
-        if (!added) {
-            this.elements.splice(0, 0, { item, priority });
-        }
+        this.elements.push({ item, priority });
     }
 
     get() {
@@ -121,7 +139,7 @@ export class PriorityQueue {
 export class Algorithms {
     /**
      * 广度优先搜索
-     * @param graph 图 
+     * @param graph 数据图表
      * @param start 起点
      * @param goal 终点
      * @returns 路径(Map)
@@ -135,7 +153,7 @@ export class Algorithms {
         while (!frontier.empty()) {
             let current = frontier.get();
 
-            if (current == goal)
+            if (current.x == goal.x && current.y == goal.y)
                 break;
 
             for (let next of graph.neighbors(current)) {
@@ -151,7 +169,7 @@ export class Algorithms {
 
     /**
      * dijkstra 算法
-     * @param graph 图
+     * @param graph 数据图表
      * @param start 起点
      * @param goal 终点
      * @returns 路径(Map)
@@ -167,15 +185,14 @@ export class Algorithms {
         while (!frontier.empty()) {
             let current = frontier.get();
 
-            if (current == goal)
+            if (current.x == goal.x && current.y == goal.y)
                 break;
 
             for (let next of graph.neighbors(current)) {
                 let new_cost = cost_so_far[`${current.x}_${current.y}`] + graph.cost(current, next);
                 if (!cost_so_far[`${next.x}_${next.y}`] || new_cost < cost_so_far[`${next.x}_${next.y}`]) {
                     cost_so_far[`${next.x}_${next.y}`] = new_cost;
-                    let priority = new_cost;
-                    frontier.put(next, priority);
+                    frontier.put(next, new_cost);
                     came_from[`${next.x}_${next.y}`] = current;
                 }
             }
@@ -209,19 +226,17 @@ export class Algorithms {
      * @param b 点b
      * @returns heuristic 值
      */
-    static heuristic(a: { x: number; y: number; }, b: { x: number; y: number; }) {
+    static heuristic(a: { x: number; y: number; }, b: { x: number; y: number; }, distance_multiplier: number) {
         // 曼哈顿距离
-        return (Math.abs(a.x - b.x) + Math.abs(a.y - b.y)) * this.distance_multiplier;
+        return (Math.abs(a.x - b.x) + Math.abs(a.y - b.y)) * distance_multiplier;
 
         // 对角距离
         // let dx = Math.abs(a.x - b.x);
         // let dy = Math.abs(a.y - b.y);
-        // return (dx + dy) * this.distance_multiplier * 1.41421;
+        // return (dx + dy) * distance_multiplier * 1.41421;
 
         // 欧几里得距离
-        // let dx = Math.abs(a.x - b.x);
-        // let dy = Math.abs(a.y - b.y);
-        // return (Math.sqrt(dx*dx + dy*dy)) * this.distance_multiplier;
+        // return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2)) * distance_multiplier;
     }
 
     /**
@@ -231,12 +246,13 @@ export class Algorithms {
 
     /**
      * A*算法
-     * @param graph 图 
+     * @param graph 数据图表
      * @param start 起点
      * @param goal 终点
+     * @param distance_multiplier heuristic 函数两个相邻点之间移动的代价，默认为1
      * @returns 路径(Map)
      */
-    static a_star_search(graph: GridWithWeights, start: { x: number; y: number; }, goal: { x: number; y: number; }) {
+    static a_star_search(graph: GridWithWeights, start: { x: number; y: number; }, goal: { x: number; y: number; }, distance_multiplier = this.distance_multiplier) {
         let frontier = new PriorityQueue();
         frontier.put(start, 0);
         let came_from = {};
@@ -247,15 +263,14 @@ export class Algorithms {
         while (!frontier.empty()) {
             let current = frontier.get();
 
-            if (current == goal)
+            if (current.x == goal.x && current.y == goal.y)
                 break;
 
             for (let next of graph.neighbors(current)) {
                 let new_cost = cost_so_far[`${current.x}_${current.y}`] + graph.cost(current, next);
                 if (!cost_so_far[`${next.x}_${next.y}`] || new_cost < cost_so_far[`${next.x}_${next.y}`]) {
                     cost_so_far[`${next.x}_${next.y}`] = new_cost;
-                    let priority = new_cost + this.heuristic(goal, next);
-                    frontier.put(next, priority);
+                    frontier.put(next, new_cost + this.heuristic(goal, next, distance_multiplier));
                     came_from[`${next.x}_${next.y}`] = current;
                 }
             }
--- assets/game/scripts/AStartPathData.ts
+++ assets/game/scripts/AStartPathData.ts
@@ -37,6 +37,18 @@ export default class AStartPathData extends cc.Component {
 
     @property([cc.Node])
     private templates: cc.Node[] = [];
+
+    @property(cc.Node)
+    private startNode: cc.Node = null;
+
+    @property(cc.Node)
+    private endNode: cc.Node = null;
+
+    @property(cc.Node)
+    private wallNode: cc.Node = null;
+
+    @property(cc.Node)
+    private weightNode: cc.Node = null;
     //#endregion
 
     private tiles: cc.Node[][] = [];
@@ -44,13 +56,18 @@ export default class AStartPathData extends cc.Component {
     // LIFE-CYCLE CALLBACKS:
     // onLoad () {}
 
-    start() {
+    protected start() {
         this.initMap();
+        this.initWalls();
+        this.initWeights();
+        this.initOptions();
         setTimeout(() => {
-            this.onSearch(0);
+            this.initStartAndEnd();
+            this.updateSearch();
         }, 1);
     }
 
+    //#region 绘制路径相关
     private initMap() {
         let row_max = 20;
         let col_max = 20;
@@ -59,7 +76,8 @@ export default class AStartPathData extends cc.Component {
             for (let col = 0; col < col_max; col++) {
                 let node = cc.instantiate(this.templates[0]);
                 node.on(cc.Node.EventType.TOUCH_START, () => {
-                    console.log(`${col},${row}`);
+                    //console.log(`${col},${row}`);
+                    this.setOptions({ x: col, y: row });
                 }, this);
                 this.mapLayer.addChild(node);
                 if (!this.tiles[col])
@@ -78,42 +96,189 @@ export default class AStartPathData extends cc.Component {
         }
     }
 
-    private initWalls(walls: { x: number, y: number }[]) {
-        for (let i = 0; i < walls.length; i++) {
-            let wall = walls[i];
-            let tile = this.tiles[wall.x][wall.y];
-            tile.color = cc.Color.GRAY;
-        }
+    private _walls = [];
+    private initWalls() {
+        let walls = [];
+        let p = (x: number, y: number) => { return { x: x, y: y } };
+        walls.push(p(3, 10), p(4, 10), p(5, 10), p(6, 10), p(7, 10), p(8, 10), p(9, 10), p(10, 10), p(11, 10), p(12, 10), p(13, 10), p(14, 10), p(15, 10));
+        walls.push(p(15, 9), p(15, 8), p(15, 7), p(15, 6), p(15, 5), p(15, 4), p(15, 3));
+
+        this.resetWall(...walls);
+
+        this._walls = walls;
     }
 
-    private initWeights(weights: { x: number, y: number }[]) {
-        for (let i = 0; i < weights.length; i++) {
-            let weight = weights[i];
-            let tile = this.tiles[weight.x][weight.y];
-            tile.color = cc.Color.GREEN;
-        }
+    private _weights = [];
+    private _w = 2;
+    private initWeights() {
+        let weights = [];
+        let p2 = (x: number, y: number, w: number) => { return { x: x, y: y, weight: w } };
+        weights.push(p2(7, 16, this._w), p2(8, 16, this._w));
+        weights.push(p2(7, 7, this._w), p2(6, 7, this._w), p2(5, 7, this._w), p2(4, 7, this._w), p2(3, 7, this._w));
+        weights.push(p2(7, 6, this._w), p2(7, 5, this._w), p2(7, 4, this._w), p2(7, 3, this._w));
+
+        this.resetWeight(...weights);
+
+        this._weights = weights;
     }
 
-    private setStartEndLayer(start: { x: number, y: number }, end: { x: number, y: number }) {
-        this.startEndLayer.removeAllChildren();
+    private _start = p(0, 0);
+    private _end = p(0, 0);
+    private initStartAndEnd() {
+        this._start = p(0, 0);
+        this._end = p(19, 19);
 
-        let tileStart = this.tiles[start.x][start.y];
-        let tileEnd = this.tiles[end.x][end.y];
+        this.resetStartNode(this._start);
+        this.resetEndNode(this._end);
+    }
 
-        let node = cc.instantiate(this.templates[3]);
-        node.on(cc.Node.EventType.TOUCH_START, () => {
-            console.log(`${start.x},${start.y}`);
+    private initOptions() {
+        this._options = [];
+        this._options.push(this.startNode);
+        this._options.push(this.endNode);
+        this._options.push(this.wallNode);
+        this._options.push(this.weightNode);
+
+        this.startNode.on(cc.Node.EventType.TOUCH_START, () => {
+            this.animationOptions(0);
         }, this);
-        this.startEndLayer.addChild(node);
-        node.setPosition(tileStart.getPosition());
 
-        node = cc.instantiate(this.templates[4]);
-        node.on(cc.Node.EventType.TOUCH_START, () => {
-            console.log(`${end.x},${end.y}`);
+        this.endNode.on(cc.Node.EventType.TOUCH_START, () => {
+            this.animationOptions(1);
+        }, this);
+
+        this.wallNode.on(cc.Node.EventType.TOUCH_START, () => {
+            this.animationOptions(2);
+        }, this);
+
+        this.weightNode.on(cc.Node.EventType.TOUCH_START, () => {
+            this.animationOptions(3);
+        }, this);
+    }
+
+    private _options: cc.Node[] = [];
+    private _currentOption: number = -1;
+    private animationOptions(index: number) {
+        this._options.forEach((option) => {
+            option.stopAllActions();
+            option.scale = 1;
+        });
+
+        if (this._currentOption == index) {
+            this._currentOption = -1;
+            return;
         }
-            , this);
-        this.startEndLayer.addChild(node);
-        node.setPosition(tileEnd.getPosition());
+        this._currentOption = index;
+
+        let option = this._options[index];
+        cc.tween(option)
+            .sequence(cc.scaleTo(0.5, 1.2), cc.scaleTo(0.5, 1))
+            .repeatForever()
+            .start();
+    }
+
+    private setOptions(pos: { x: number, y: number }) {
+        if (this._currentOption == -1) return;
+
+        switch (this._currentOption) {
+            case 0:
+                this.resetStartNode(pos);
+                break;
+            case 1:
+                this.resetEndNode(pos);
+                break;
+            case 2:
+                this.resetWall(pos);
+                break;
+            case 3:
+                this.resetWeight(pos);
+                break;
+        }
+
+        this.updateSearch();
+    }
+
+    private _startNode = null;
+    private resetStartNode(pos: { x: number, y: number }) {
+        if (!this._startNode) {
+            this._startNode = cc.instantiate(this.templates[3]);
+            this.startEndLayer.addChild(this._startNode);
+        }
+        this._startNode.position = this.tiles[pos.x][pos.y].position;
+        this._start = pos;
+    }
+
+    private _endNode = null;
+    private resetEndNode(pos: { x: number, y: number }) {
+        if (!this._endNode) {
+            this._endNode = cc.instantiate(this.templates[4]);
+            this.startEndLayer.addChild(this._endNode);
+        }
+        this._endNode.position = this.tiles[pos.x][pos.y].position;
+        this._end = pos;
+    }
+
+    private resetWall(...potions: { x: number, y: number }[]) {
+        if (potions.length == 0) return;
+        if (potions.length == 1) {
+            let pos = potions[0];
+            if (this.tiles[pos.x][pos.y].color.equals(this.wallNode.color)) {
+                this.tiles[pos.x][pos.y].color = cc.Color.WHITE;
+
+                // 从中移除_walls
+                for (const iterator of this._walls) {
+                    if (iterator.x == pos.x && iterator.y == pos.y) {
+                        this._walls.splice(this._walls.indexOf(iterator), 1);
+                        break;
+                    }
+                }
+            }
+            else {
+                this.tiles[pos.x][pos.y].color = this.wallNode.color;
+
+                // 加入_walls
+                this._walls.push(pos);
+            }
+
+            this.updateSearch();
+        }
+        else {
+            potions.forEach((pos) => {
+                this.tiles[pos.x][pos.y].color = this.wallNode.color;
+            });
+        }
+    }
+
+    private resetWeight(...potions: { x: number, y: number }[]) {
+        if (potions.length == 0) return;
+        if (potions.length == 1) {
+            let pos = potions[0];
+            if (this.tiles[pos.x][pos.y].color.equals(this.weightNode.color)) {
+                this.tiles[pos.x][pos.y].color = cc.Color.WHITE;
+
+                // 从中移除_weights
+                for (const iterator of this._weights) {
+                    if (iterator.x == pos.x && iterator.y == pos.y) {
+                        this._weights.splice(this._weights.indexOf(iterator), 1);
+                        break;
+                    }
+                }
+            }
+            else {
+                this.tiles[pos.x][pos.y].color = this.weightNode.color;
+
+                // 加入_weights
+                //let p2 = (x: number, y: number, w: number) => { return { x: x, y: y, weight: w } };
+                this._weights.push({x: pos.x, y: pos.y, weight: this._w});
+            }
+
+            this.updateSearch();
+        }
+        else {
+            potions.forEach((pos) => {
+                this.tiles[pos.x][pos.y].color = this.weightNode.color;
+            });
+        }
     }
 
     private setPathLayer(path: Position[]) {
@@ -132,9 +297,9 @@ export default class AStartPathData extends cc.Component {
     private setNumberLayer(pathMap: {}, start: Position) {
         this.numberLayer.removeAllChildren();
 
+        let tmpNumberMap = {};
         let array: any[] = [];
         array.push({ x: start.x, y: start.y, level: 0 });
-        let pre = `${start.x}_${start.y}`;
         while (array.length > 0) {
             let current = array.shift();
             for (const _key in pathMap) {
@@ -143,14 +308,19 @@ export default class AStartPathData extends cc.Component {
                     let arr = _key.split("_");
                     array.push({ x: Number(arr[0]), y: Number(arr[1]), level: current.level + 1 });
 
-                    let node = cc.instantiate(this.templates[2]);
-                    this.numberLayer.addChild(node);
-                    node.setPosition(this.tiles[next.x][next.y].getPosition());
-                    node.getComponent(cc.Label).string = `${current.level + 1}`;
+                    if (!tmpNumberMap[`${next.x}_${next.y}`]) {
+                        let node = cc.instantiate(this.templates[2]);
+                        this.numberLayer.addChild(node);
+                        node.setPosition(this.tiles[next.x][next.y].getPosition());
+                        node.getComponent(cc.Label).string = `${current.level + 1}`;
+                        tmpNumberMap[`${next.x}_${next.y}`] = true;
+                    }
                 }
             }
         }
+        tmpNumberMap = {};
     }
+    //#endregion
 
     private breadthFirstSearch(width: number, height: number, walls: Position[], start: Position, end: Position) {
         let graph = new SquareGrid(width, height, walls);
@@ -179,25 +349,12 @@ export default class AStartPathData extends cc.Component {
         this.setPathLayer(path);
     }
 
-    private onSearch(type: number) {
-        let walls = [];
-        let p = (x: number, y: number) => { return { x: x, y: y } };
-        walls.push(p(3, 10), p(4, 10), p(5, 10), p(6, 10), p(7, 10), p(8, 10), p(9, 10), p(10, 10), p(11, 10), p(12, 10), p(13, 10), p(14, 10), p(15, 10));
-        walls.push(p(15, 9), p(15, 8), p(15, 7), p(15, 6), p(15, 5), p(15, 4), p(15, 3));
-
-        let weights = [];
-        let p2 = (x: number, y: number, w: number) => { return { x: x, y: y, weight: w } };
-        weights.push(p2(7, 16, 2), p2(8, 16, 2));
-
-        let start = p(2, 3);
-        let end = p(19, 19);
-
-        this.resetMap();
-        this.initWalls(walls);
-        this.initWeights(weights);
-        this.setStartEndLayer(start, end);
-
-        switch (type) {
+    private updateSearch() {
+        let walls = this._walls;
+        let weights = this._weights;
+        let start = this._start;
+        let end = this._end;
+        switch (this._currentType) {
             case 0:
                 this.breadthFirstSearch(20, 20, walls, start, end);
                 break;
@@ -210,18 +367,11 @@ export default class AStartPathData extends cc.Component {
         }
     }
 
+    private _currentType: number = 0;
     onToggleEvent(toggle: cc.Toggle) {
         let name = toggle.node.name;
-        switch (name) {
-            case "1":
-                this.onSearch(0);
-                break;
-            case "2":
-                this.onSearch(1);
-                break;
-            case "3":
-                this.onSearch(2);
-        }
+        this._currentType = Number(name);
+        this.updateSearch();
     }
 
     // update (dt) {}
