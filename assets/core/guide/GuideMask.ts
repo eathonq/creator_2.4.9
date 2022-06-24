564cafb383481e963683d57566ed83be3f3838e5
eathonq <vangagh@live.cn>
2022-06-24 16:43:13 +0800

    GuideMask temp

--- assets/core/guide/GuideMask.ts
+++ assets/core/guide/GuideMask.ts
@@ -12,10 +12,44 @@ const { ccclass, property, menu } = cc._decorator;
 @menu("guide/GuideMask")
 export default class GuideMask extends cc.Mask {
 
+    @property(cc.Node)
+    private target: cc.Node = null;
+
     // onLoad () {}
 
-    protected start() {
+    private get graphics(): cc.Graphics {
+        return this['_graphics'];
+    }
 
+    protected start() {
+        this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchCancel, this);
+
+        this.graphicsNode(this.target);
+    }
+
+    private graphicsNode(node: cc.Node) {
+        if(CC_EDITOR) return;
+
+        let grp = this.graphics;
+        let nodePos = node.convertToWorldSpaceAR(cc.Vec2.ZERO);
+        let nodeSize = node.getContentSize();
+        let rect = cc.rect(nodePos.x, nodePos.y, nodeSize.width, nodeSize.height);
+        grp.clear();
+        grp.fillRect(rect.x - 960/2, rect.y-640/2, rect.width, rect.height);
+    }
+
+    onTouchCancel(touch: cc.Event.EventTouch) {
+        console.log("onTouchCancel");
+    }
+
+    onTouchEnd(touch: cc.Event.EventTouch) {
+        console.log("onTouchEnd");
+    }
+
+    onTouchStart(touch: cc.Event.EventTouch) {
+        console.log("onTouchStart");
     }
 
     // update (dt) {}
